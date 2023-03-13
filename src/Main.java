7236a842e69561348aaad09b72d810532829c82b
indi__sid <indisid.indisid@gmail.com>
2023-03-13 22:38:03 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        long tiket = 456_897;
+        long bonus = tiket / 20;
+
+        System.out.println("Добрый день. Вам начислено " + bonus + " миль(ли)");
+
+
+        }
+}
